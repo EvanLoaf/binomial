ec11158440338762f8f6306a716d00dae2f8aa7f
Ivan Karavai <ikaravai@solvd.com>
2023-01-14 19:03:35 +0300

    Implement calculation logic for natural n, validation for coefficients

--- pages/binomial.tsx
+++ pages/binomial.tsx
@@ -17,34 +17,66 @@ const Binomial = () => {
 
   const handleSubmit = async (e: { preventDefault: () => void; }) => {
     e.preventDefault();
+    setError('');
+    setResult('');
 
     let a: number = Number(a_coef);
     let b: number = Number(b_coef);
     let n: number = Number(n_coef);
 
     // Validate input
-    if (isNaN(Number(a)) || isNaN(Number(b)) || isNaN(Number(n))) {
+    if (isNaN(a) || isNaN(b) || isNaN(n)) {
       setError("Please enter valid numbers for coefficients a, b and n.");
       return;
     }
 
     // Validate input
-    if (isNaN(Number(a)) || isNaN(Number(b)) || isNaN(Number(n))) {
-      setError("Please enter valid numbers for coefficients a, b and n.");
+    if (a > 1000 || b > 1000 || n > 1000) {
+      setError("Values cannot be over 1000 due to limited computing power");
       return;
     }
 
-    // Calculate result
-    let k = 1;
-    let sum = 0;
-
-    for (let i = 1; i <= n + 1; i++) {
-      sum += k * (b ** (n - i + 1)) * (a ** (i - 1));
-      k = k * (n - i + 2) / (i - 1);
+    if ((!Number.isInteger(n) || n < 1) && b !== 1) {
+      setError("For non-natural n, only b = 1 accepted");
+      return;
     }
-    let number = 5;
-    setResult(number.toString(10));
+
+    let res: string = '';
+
+    if (!Number.isInteger(n) || n < 1) {
+      res = CalculateForNonNaturalN(a, b, n);
+    } else {
+      res = CalculateForNaturalN(a, b, n);
+    }
+
+    setResult(res);
   };
+
+  const CalculateForNaturalN = (a : number, b : number, n : number) : string => {
+    let i : number = 1;
+    let k : number = 1;
+    return CalcTerm(a, b, n, i, k);
+  }
+
+  const CalculateForNonNaturalN = (a : number, b : number, n : number) : string => {
+    return 'Not yet implemented for non-natural n';
+  }
+
+  const CalcTerm = (a : number, b : number, n : number, i : number, k : number) : string => {
+    let prev_result !: string;
+    let next_i : number = i + 1;
+    let next_k : number = k * (n - (next_i - 2)) / (next_i - 1);
+    if (i < n + 1) {
+      prev_result = CalcTerm(a, b, n, next_i, next_k);
+    }
+    let x_coef : number = k * (b ** (n - i + 1)) * (a ** (i - 1));
+    console.log('k = ' + k + ', i = ' + i + ', a = ' + a + ', b = ' + b + ', n = ' + n);
+    console.log(x_coef);
+    let term_result : string = x_coef + 'x^' + (i - 1);
+    if (prev_result != null) {
+      return prev_result.concat(' + ').concat(term_result);
+    } else return term_result;
+  }
 
   return (
     <div>
